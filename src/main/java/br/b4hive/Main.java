bec8eac3cd2f509d8319eb3412c58b8a426d67fb
Bruno dos Santos Silva <142647702+B4Hive@users.noreply.github.com>
2024-11-16 02:44:32 -0300

    small edit on version and temp message

--- src/main/java/br/b4hive/Main.java
+++ src/main/java/br/b4hive/Main.java
@@ -1,7 +1,11 @@
 package br.b4hive;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("Lone BeeHive: Megachile's Journey");
+
     }
+
 }
